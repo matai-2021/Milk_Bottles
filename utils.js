72aca6fad944cc00b8e7d3d7eefb7543712532f9
Ryan Bosher <41498159+bosh-code@users.noreply.github.com>
2021-06-11 14:44:28 +1200

    finish logic code

--- utils.js
+++ utils.js
@@ -1,29 +1,48 @@
-function getResult (score) {
-  let result = {}
-  if (score => 0 && score <= 3) {
-    result = {
+const { getAllAnswers } = require('./db/db')
+
+function getResult (input) {
+  const score = input
+  if (score > -1 && score < 4) {
+    console.log('IN LUMP')
+
+    return {
       name: 'Pineappple Lump',
       img: 'images/pineapple.jpg'
     }
-  } else if (score => 4 && score <= 7) {
-    result = {
+  } else if (score > 3 && score < 8) {
+    console.log('IN RIPE')
+
+    return {
       name: 'Cherry Ripe',
       img: 'images/cherry.jpg'
     }
-  } else if (score => 8 && score <= 11) {
-    result = {
+  } else if (score > 7 && score < 12) {
+    console.log('IN PEACH')
+
+    return {
       name: 'Sour Peach',
       img: 'images/peach.jpg'
     }
-  } else if (score => 11 && score <= 15) {
-    result = {
+  } else if (score > 10 && score < 16) {
+    console.log('IN MILK')
+
+    return {
       name: 'Milk Bottle',
       img: 'images/milk.jpg'
     }
   }
-  return result
+}
+
+async function getAnswersArr () {
+  const allAnswers = await getAllAnswers()
+  const returnArr = []
+  for (let i = 0; i < allAnswers.length; i++) {
+    returnArr.push(Object.values(allAnswers[i])[0])
+  }
+  return returnArr
 }
 
 module.exports = {
-  getResult
+  getResult,
+  getAnswersArr
 }
